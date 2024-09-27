523e896daab043c7189f3a0e4ff8f8c677b4faca
Tanmay <100037804+tanmay233@users.noreply.github.com>
2024-09-28 02:20:43 +0530

    Update settings.py

--- irctc_booking/settings.py
+++ irctc_booking/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'irctc_booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
-#         'NAME'    : 'irctc',                 # <-- UPDATED line 
-#         'USER'    : 'root',                     # <-- UPDATED line
-#         'PASSWORD': '',              # <-- UPDATED line
-#         'HOST'    : 'localhost',                # <-- UPDATED line
-#         'PORT'    : '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# using mysql localhost
+DATABASES = {
+    'default': {
+        'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
+        'NAME'    : 'irctc',                 # <-- UPDATED line 
+        'USER'    : 'root',                     # <-- UPDATED line
+        'PASSWORD': '',              # <-- UPDATED line
+        'HOST'    : 'localhost',                # <-- UPDATED line
+        'PORT'    : '3306',
+    }
+}
 
 
 # Password validation
